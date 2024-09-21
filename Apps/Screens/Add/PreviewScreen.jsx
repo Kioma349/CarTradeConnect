643a21752d69201599fd594628674a4eb12f39ce
Linus <117490184+Kioma349@users.noreply.github.com>
2024-09-21 23:09:01 +0200

    Premier affichage de l'image avec Add Details en "Top"

--- Apps/Screens/Add/PreviewScreen.jsx
+++ Apps/Screens/Add/PreviewScreen.jsx
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React, { useEffect } from 'react'
 import { useRoute } from '@react-navigation/native'
+import Colors from '../../Utils/Colors'
+
 
 export default function PreviewScreen() {
 
@@ -16,8 +18,29 @@ export default function PreviewScreen() {
 
   
   return (
-    <View>
-      <Text>PreviewScreen</Text>
+    <View style={{padding: 20}}>
+      <View style={{
+        alignItems: 'center',
+        marginTop: 45,
+      }}>
+            <Text 
+            style={{ 
+              fontFamily:'outfit-bold', 
+              fontSize:20}} 
+              
+              > Add Details</Text>
+
+              <Image source={{ uri:params?.thumbnail }}
+              style={{
+                width: 200,
+                height: 300,
+                borderRadius: 15,
+                marginTop: 15
+                
+              }}/>
+
+      </View>
+      
     </View>
   )
 }
