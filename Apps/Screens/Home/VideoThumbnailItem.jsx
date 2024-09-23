9b7f8be0fe34552d5edaf32b57e187e9791ecdb4
Linus <117490184+Kioma349@users.noreply.github.com>
2024-09-23 13:27:31 +0200

    Ajout icon "Like"

--- Apps/Screens/Home/VideoThumbnailItem.jsx
+++ Apps/Screens/Home/VideoThumbnailItem.jsx
@@ -1,36 +1,54 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import Colors from '../../Utils/Colors'
-import { supabase } from '../../Utils/SupabaseConfig'
-import { s3bucket } from '../../Utils/S3BucketConfig';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function VideoThumbnailItem({video}) {
-  return (
-    <View style={{flex:1, margin:5}}>
-      <View style={{position:'absolute', zIndex:10, bottom:0, padding:5}}>
 
-        <View>
-
-            <Image source={{uri:video?.Users?.profileImage}}
-            style={{width:20, height:20, backgroundColor:Colors.WHITE, borderRadius:99}}
+export default function VideoThumbnailItem({ video }) {
+    return (
+        <View style={{ flex: 1, margin: 5 }}>
             
+            <View style={{ position: 'absolute', zIndex: 10, bottom: 0, padding: 5 }}>
+
+                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+
+                    <Image source={{ uri: video?.Users?.profileImage }}
+                        style={{ width: 20, height: 20, backgroundColor: Colors.WHITE, borderRadius: 99 }}
+
+                    />
+                    
+                    <Text style={{
+                        color: Colors.WHITE,
+                        fontFamily: 'outfit-bold', fontSize: 12, marginLeft: 5, marginTop: 5, textShadowColor: Colors.BLACK
+                    }}>{video?.Users?.username}</Text>
+
+                    
+                </View>
+
+                <View>
+
+                    <Text style={{fontFamily:'outfit',
+                        fontSize: 12, color:Colors.WHITE}}>36</Text>
+
+                        <Ionicons name="heart-outline" size={24} color="white" />
+                </View>
+
+                
+                
+
+
+            </View>
+
+            
+            <Image source={{ uri: video?.thumbnail }}
+                style={{ width: '100%', height: 250, borderRadius: 10 }}
+
             />
-            <Text style={{color:Colors.WHITE,
-                fontFamily:'outfit-bold',fontSize:12, marginLeft:5, marginTop:5, textShadowColor:Colors.BLACK, textShadowRadius:10, textShadowOffset:{width:1, height:1}
-            }}>{video?.Users?.username}</Text>
+
+
+
+
+
         </View>
-
-
-      </View>
-    <Image source={{uri:video?.thumbnail}}
-    style = {{width:'100%', height:250, borderRadius:10}}
-
-    />
-
-
-
-
-
-    </View>
-  )
+    )
 }
