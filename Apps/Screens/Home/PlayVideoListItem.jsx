c09668fbe6d66b85b6a8636c9048b627770dbc9d
Linus <117490184+Kioma349@users.noreply.github.com>
2024-09-23 16:49:11 +0200

    BottomTabHeight

--- Apps/Screens/Home/PlayVideoListItem.jsx
+++ Apps/Screens/Home/PlayVideoListItem.jsx
@@ -1,16 +1,21 @@
 import React, { useRef, useState } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 
 export default function PlayVideoListItem({ video }) {
   const videoRef = useRef(null);
   const [status, setStatus] = useState({});
 
+  const BottomTabHeight= useBottomTabBarHeight();
+
+  const ScreenHeight = Dimensions.get('window').height-BottomTabHeight;
+
   return (
     <View style={styles.container}>
       <Video
         ref={videoRef}
-        style={styles.video}
+        style={[styles.video, {height: ScreenHeight}]}
         source={{ uri: video?.videoUrl }}
         useNativeControls
         resizeMode={ResizeMode.COVER}
